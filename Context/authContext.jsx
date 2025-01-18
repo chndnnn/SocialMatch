b90e1a0bd789effe8c68ecde47e567509d69b09d
chndnnn <chndn136@gmail.com>
2025-01-18 15:02:14 +0530

    redirecting to home page

--- Context/authContext.jsx
+++ Context/authContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useState } from "react";
 
 const authContext = createContext(undefined);
 
-const authContextProvider = ({ children }) => {
+const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
   const setAuth = (userData) => {
@@ -23,9 +23,9 @@ const authContextProvider = ({ children }) => {
 export const authState = () => {
   const context = useContext(authContext);
   if (!context) {
-    throw new Error("useChatState must be used within a ChatssProvider");
+    throw new Error("authState must be used within a authContextProvider");
   }
   return context;
 };
 
-export default authContextProvider;
+export default AuthContextProvider;
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,29 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import "../global.css";
+import AuthContextProvider, { authState } from "../Context/authContext";
+import { useEffect } from "react";
+import { supabase } from "../lib/supabase";
 
-const _Layout = () => {
+const _layout = () => {
+  return (
+    <AuthContextProvider>
+      <MainLayout />
+    </AuthContextProvider>
+  );
+};
+
+const MainLayout = () => {
+  const router = useRouter();
+  const { user } = authState();
+  useEffect(() => {
+    console.log(user);
+    supabase.auth.onAuthStateChange((_event, session) => {
+      if (session) {
+        router.replace("/home");
+      }
+    });
+  }, []);
   return <Stack screenOptions={{ headerShown: false }}></Stack>;
 };
 
-export default _Layout;
+export default _layout;
