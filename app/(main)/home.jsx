338121ced3c891d33a53dc0cc03bff0934be0123
chndnnn <chndn136@gmail.com>
2025-02-04 22:56:57 +0530

    removed unwanted code

--- app/(main)/home.jsx
+++ app/(main)/home.jsx
@@ -1,16 +1,14 @@
 import { Pressable, Text } from "react-native";
 import { View } from "react-native";
 import { authState } from "../../Context/authContext";
-import { useRouter } from "expo-router";
 import ScreenWrapper from "../../components/ScreenWraper";
 import { supabase } from "../../lib/supabase";
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import CustomInput from "../../components/CustomInput";
 
 const Home = () => {
   const { user, setAuth } = authState();
   const [phone, setPhone] = useState();
-  const router = useRouter();
   function onLogutClick() {
     setAuth(null);
     const { error } = supabase.auth.signOut();
